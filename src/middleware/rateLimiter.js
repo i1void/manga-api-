bd32df50294e71196785041a2a09ad23d58cf0d0
Void <152130497+i1void@users.noreply.github.com>
2026-03-11 19:36:06 +0700

    .

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -1,1 +1,13 @@
+import rateLimit from "express-rate-limit";
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 200,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    error: "Terlalu banyak request dari IP ini, silakan coba lagi setelah 15 menit",
+  },
+});
+
+export default limiter;
